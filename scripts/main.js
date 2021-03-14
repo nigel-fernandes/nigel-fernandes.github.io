d50691872001d8c43961005f764ef5fce822d964
Nigel Fernandes <nigelfds@outlook.com>
2021-03-14 18:59:28 +1100

    add changes from old design

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    $('console-input').focus();
+
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: true
+        //smoothHeight: true,
+        //allowOneSlide: true
+    });
+    //$('#slider').flexslider("play");
+});
+
+//$(document).ready(function () {
+
+
+
+    //$("[href]").each(function () {
+    //    if (this.href == window.location.href) {
+    //        $(this).parent("li").addClass("current-link");
+    //    }
+    //});
+//});
+
+
